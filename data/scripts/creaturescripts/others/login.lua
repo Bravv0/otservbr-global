65c02687005b8c4363f4d193a1e5dace0c458e33
Bravv0 <38669221+Bravv0@users.noreply.github.com>
2020-10-19 16:59:43 -0300

    Give some quests/access automatically.

--- data/scripts/creaturescripts/others/login.lua
+++ data/scripts/creaturescripts/others/login.lua
@@ -145,6 +145,142 @@ function playerLogin.onLogin(player)
 	if player:getStorageValue(Storage.isTraining) == 1 then --Reset exercise weapon storage
 		player:setStorageValue(Storage.isTraining,0)
 	end
+	
+	giveQuests(player) -- give access to quests automatically
 	return true
 end
+
+function checkQuest(player, storage, value)
+	if player:getStorageValue(storage) < value then 
+		player:setStorageValue(storage, value)
+	end
+end
+
+function giveQuests(player)
+--Quests Liberadas
+--In Service of Yalahar 
+	checkQuest(player, Storage.isTraining, 0)
+	checkQuest(player, Storage.InServiceofYalahar.Questline, 51)
+	checkQuest(player, Storage.InServiceofYalahar.Mission01, 6)
+	checkQuest(player, Storage.InServiceofYalahar.Mission02, 8)
+	checkQuest(player, Storage.InServiceofYalahar.Mission03, 6)
+	checkQuest(player, Storage.InServiceofYalahar.Mission04, 6)
+	checkQuest(player, Storage.InServiceofYalahar.Mission05, 8)
+	checkQuest(player, Storage.InServiceofYalahar.Mission06, 5)
+	checkQuest(player, Storage.InServiceofYalahar.Mission07, 5)
+	checkQuest(player, Storage.InServiceofYalahar.Mission08, 4)
+	checkQuest(player, Storage.InServiceofYalahar.Mission09, 2)
+	checkQuest(player, Storage.InServiceofYalahar.Mission10, 1)
+	checkQuest(player, Storage.InServiceofYalahar.DoorToLastFight, 1)
+	
+--WOTE
+	checkQuest(player, Storage.WrathoftheEmperor.Questline, 1)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission01, 3)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission02, 3)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission03, 3)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission04, 3)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission05, 3)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission06, 4)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission07, 6)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission08, 2)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission09, 2)
+	checkQuest(player, Storage.WrathoftheEmperor.Mission10, 1)
+--Imbuiment
+	checkQuest(player, Storage.ForgottenKnowledge.Tomes, 1)
+	checkQuest(player, Storage.ForgottenKnowledge.LastLoreKilled, 1)    
+	checkQuest(player, Storage.ForgottenKnowledge.TimeGuardianKilled, 1)
+	checkQuest(player, Storage.ForgottenKnowledge.HorrorKilled, 1)
+	checkQuest(player, Storage.ForgottenKnowledge.DragonkingKilled, 1)
+	checkQuest(player, Storage.ForgottenKnowledge.ThornKnightKilled, 1)
+	checkQuest(player, Storage.ForgottenKnowledge.LloydKilled, 1)
+	checkQuest(player, Storage.ForgottenKnowledge.LadyTenebrisKilled, 1)
+	checkQuest(player, Storage.ForgottenKnowledge.AccessMachine, 1)
+--SearoutsYalahar
+	checkQuest(player, Storage.SearoutesAroundYalahar.Darashia, 1)
+	checkQuest(player, Storage.SearoutesAroundYalahar.AbDendriel, 1)
+	checkQuest(player, Storage.SearoutesAroundYalahar.Venore, 1)
+	checkQuest(player, Storage.SearoutesAroundYalahar.Ankrahmun, 1)
+	checkQuest(player, Storage.SearoutesAroundYalahar.PortHope, 1)
+	checkQuest(player, Storage.SearoutesAroundYalahar.Thais, 1)
+	checkQuest(player, Storage.SearoutesAroundYalahar.LibertyBay, 1)
+	checkQuest(player, Storage.SearoutesAroundYalahar.Carlin, 1)	
+	
+--The Ape City
+	checkQuest(player, Storage.TheApeCity.Started, 1)	
+	checkQuest(player, Storage.TheApeCity.Questline, 18)
+	checkQuest(player, Storage.TheApeCity.DworcDoor, 1)
+	checkQuest(player, Storage.TheApeCity.ChorDoor, 1)
+	checkQuest(player, Storage.TheApeCity.ParchmentDecyphering, 1)
+	checkQuest(player, Storage.TheApeCity.FibulaDoor, 1)
+	checkQuest(player, Storage.TheApeCity.WitchesCapSpot, 1)
+	checkQuest(player, Storage.TheApeCity.CasksDoor, 1)
+	checkQuest(player, Storage.TheApeCity.Casks, 1)
+	checkQuest(player, Storage.TheApeCity.HolyApeHair, 1)
+	checkQuest(player, Storage.TheApeCity.SnakeDestroyer, 1)
+	
+--The Inquisition
+	checkQuest(player, Storage.TheInquisition.Questline, 20)
+	checkQuest(player, Storage.TheInquisition.Mission01, 7)
+	checkQuest(player, Storage.TheInquisition.Mission02, 3)
+	checkQuest(player, Storage.TheInquisition.Mission03, 6)
+	checkQuest(player, Storage.TheInquisition.Mission04, 3)
+	checkQuest(player, Storage.TheInquisition.Mission05, 3)
+	checkQuest(player, Storage.TheInquisition.Mission06, 3)
+	
+--Djinns
+	checkQuest(player, Storage.DjinnWar.EfreetFaction.Start, 1)
+	checkQuest(player, Storage.DjinnWar.EfreetFaction.Mission01, 3)
+	checkQuest(player, Storage.DjinnWar.EfreetFaction.Mission02, 3)
+	checkQuest(player, Storage.DjinnWar.EfreetFaction.Mission03, 3)
+
+	checkQuest(player, Storage.DjinnWar.MaridFaction.Start, 1)
+	checkQuest(player, Storage.DjinnWar.MaridFaction.Mission01, 2)
+	checkQuest(player, Storage.DjinnWar.MaridFaction.Mission02, 2)
+	checkQuest(player, Storage.DjinnWar.MaridFaction.RataMari, 2)
+	checkQuest(player, Storage.DjinnWar.MaridFaction.Mission03, 3)
+	checkQuest(player, Storage.DjinnWar.Faction.Greeting, 2)
+	checkQuest(player, Storage.DjinnWar.Faction.EfreetDoor, 1)
+	checkQuest(player, Storage.DjinnWar.Faction.MaridDoor, 1)
+	
+--post man quest
+	checkQuest(player, Storage.Postman.Mission01, 6)
+	checkQuest(player, Storage.Postman.Mission02, 3)
+	checkQuest(player, Storage.Postman.Mission03, 3)
+	checkQuest(player, Storage.Postman.Mission04, 2)
+	checkQuest(player, Storage.Postman.Mission05, 4)
+	checkQuest(player, Storage.Postman.Mission06, 13)
+	checkQuest(player, Storage.Postman.Mission07, 8)
+	checkQuest(player, Storage.Postman.Mission08, 3)
+	checkQuest(player, Storage.Postman.Mission09, 4)
+	checkQuest(player, Storage.Postman.Mission10, 3)
+	checkQuest(player, Storage.Postman.Rank, 5)
+	checkQuest(player, Storage.Postman.Door, 1)
+	
+--warzones 
+	checkQuest(player, Storage.BigfootBurden.QuestLine, 30)
+	checkQuest(player, Storage.BigfootBurden.QuestLineComplete, 2)
+	checkQuest(player, Storage.BigfootBurden.Shooting, 5)
+	checkQuest(player, Storage.BigfootBurden.Rank, 1450)
+	checkQuest(player, Storage.BigfootBurden.Warzone1Access, 2)
+	checkQuest(player, Storage.BigfootBurden.Warzone2Access, 2)
+	checkQuest(player, Storage.BigfootBurden.Warzone3Access, 2)
+	checkQuest(player, Storage.BigfootBurden.WarzoneStatus, 1)
+	
+	
+--the new frontier, farmine access
+	checkQuest(player, Storage.TheNewFrontier.Questline, 25)
+	checkQuest(player, Storage.TheNewFrontier.Mission01, 3)
+	checkQuest(player, Storage.TheNewFrontier.Mission02, 6)
+	checkQuest(player, Storage.TheNewFrontier.Mission03, 3)
+	checkQuest(player, Storage.TheNewFrontier.Mission04, 2)
+	checkQuest(player, Storage.TheNewFrontier.Mission05, 7)
+	checkQuest(player, Storage.TheNewFrontier.Mission06, 3)
+	checkQuest(player, Storage.TheNewFrontier.Mission07, 3)
+	checkQuest(player, Storage.TheNewFrontier.Mission08, 2)
+	checkQuest(player, Storage.TheNewFrontier.Mission09, 3)
+	checkQuest(player, Storage.TheNewFrontier.Mission10, 1) 
+	checkQuest(player, Storage.TheNewFrontier.TomeofKnowledge, 12)
+	checkQuest(player, Storage.TheNewFrontier.TomeofKnowledge, 12)	
+end
+
 playerLogin:register()
