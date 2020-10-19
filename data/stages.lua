786f0621a4bdf722a8fdf95501f0b2b824f7dd63
Bravv0 <38669221+Bravv0@users.noreply.github.com>
2020-10-19 17:00:02 -0300

    XP/Skill/ML Stages update

--- data/stages.lua
+++ data/stages.lua
@@ -5,71 +5,87 @@ experienceStages = {
 	{
 		minlevel = 1,
 		maxlevel = 8,
-		multiplier = 7
-	}, {
+		multiplier = 30
+	},
+	{
 		minlevel = 9,
-		maxlevel = 20,
-		multiplier = 6
-	}, {
-		minlevel = 21,
-		maxlevel = 50,
-		multiplier = 5
-	}, {
-		minlevel = 51,
-		maxlevel = 100,
+		maxlevel = 45,
+		multiplier = 30
+	},
+	{
+		minlevel = 46,
+		maxlevel = 80,
+		multiplier = 15
+	},
+	{
+		minlevel = 81,
+		maxlevel = 130,
+		multiplier = 10
+	},
+	{
+		minlevel = 131,
+		maxlevel = 160,
+		multiplier = 7
+	},
+	{
+		minlevel = 161,
+		maxlevel = 190,
 		multiplier = 4
-	}, {
-		minlevel = 101,
+	},
+		{
+		minlevel = 191,
+		maxlevel = 220,
 		multiplier = 2
+	},
+		{
+		minlevel = 221,
+		maxlevel = 250,
+		multiplier = 1.5
+	},
+		{
+		minlevel = 251,
+		multiplier = 1
 	}
 }
 
 skillsStages = {
 	{
 		minlevel = 10,
-		maxlevel = 60,
-		multiplier = 15
+		maxlevel = 59,
+		multiplier = 20
 	}, {
-		minlevel = 61,
-		maxlevel = 80,
+		minlevel = 60,
+		maxlevel = 69,
 		multiplier = 10
 	}, {
-		minlevel = 81,
-		maxlevel = 110,
-		multiplier = 6
+		minlevel = 70,
+		maxlevel = 79,
+		multiplier = 5
 	}, {
-		minlevel = 111,
-		maxlevel = 125,
+		minlevel = 80,
+		maxlevel = 99,
 		multiplier = 4
 	}, {
-		minlevel = 126,
-		multiplier = 2
+		minlevel = 100,
+		multiplier = 1
 	}
 }
 
 magicLevelStages = {
 	{
 		minlevel = 0,
-		maxlevel = 60,
-		multiplier = 10
+		maxlevel = 59,
+		multiplier = 15
 	}, {
-		minlevel = 61,
-		maxlevel = 80,
-		multiplier = 7
-	}, {
-		minlevel = 81,
-		maxlevel = 100,
+		minlevel = 60,
+		maxlevel = 79,
 		multiplier = 5
 	}, {
-		minlevel = 101,
-		maxlevel = 110,
-		multiplier = 4
-	}, {
-		minlevel = 111,
-		maxlevel = 125,
-		multiplier = 3
-	}, {
-		minlevel = 126,
+		minlevel = 80,
+		maxlevel = 89,
 		multiplier = 2
+	}, {
+		minlevel = 90,
+		multiplier = 1
 	}
 }
