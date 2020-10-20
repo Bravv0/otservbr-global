e0a7e7a1461817f2694251e6ae698414444cf8b5
Bravv0 <38669221+Bravv0@users.noreply.github.com>
2020-10-20 09:36:05 -0300

    Custom startup script created.

--- data/scripts/custom/on_startup.lua
+++ data/scripts/custom/on_startup.lua
@@ -1,4 +1,4 @@
-local config = {
+local configTrainers = { -- trainers
 	-- Position of the first position (line 1 column 1)
 	firstRoomPos = {x = 9037, y = 12516, z = 7},
 	-- X distance between each room (on the same line)
@@ -13,35 +13,36 @@ local config = {
 
 function spawnTrainers()
 	local TrainersPos = getTrainersPos()
-	for i=1,config.columns do
-		for j=1,config.rows do			
+	for i=1,configTrainers.columns do
+		for j=1,configTrainers.rows do			
 			Game.createMonster("Training Monk", {x = TrainersPos[i][j].x-1, y = TrainersPos[i][j].y-1, z = TrainersPos[i][j].z}, true, false)
 	        Game.createMonster("Training Monk", {x = TrainersPos[i][j].x+1, y = TrainersPos[i][j].y-1, z = TrainersPos[i][j].z}, true, false)
 		end	
 	end
 end
 
-
 function getTrainersPos()
 	local array = {}
-	for i=1,config.columns do
+	for i=1,configTrainers.columns do
 		array[i] = {}	
-		for j=1,config.rows do			
-			array[i][j] = {x = config.firstRoomPos.x + (config.nextPosX*(i-1)), y = config.firstRoomPos.y + (config.nextPosY*(j-1)), z = 7}
+		for j=1,configTrainers.rows do			
+			array[i][j] = {x = configTrainers.firstRoomPos.x + (configTrainers.nextPosX*(i-1)), y = configTrainers.firstRoomPos.y + (configTrainers.nextPosY*(j-1)), z = 7}
 		end	
 	end
 	return array
 end
 
 
-local spawnerTrainer = GlobalEvent("spawn trainers")
-function spawnerTrainer.onStartup()
+local customStartup = GlobalEvent("spawn trainers")
+function customStartup.onStartup()
 	addEvent(function()
-				spawnTrainers()
-				print("> Spawned Training Monks.")
-			end, 7 * 1000)
+		spawnTrainers()
+		print("> Spawned Training Monks.")
+	end, 5 * 1000)
+	
+	print(Game.getLastServerSave())
 end
 
 
-spawnerTrainer:register()
+customStartup:register()
 
